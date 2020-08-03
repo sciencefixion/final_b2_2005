abf5eaad3e1d81a3bbd49285273677036b00dacc
Rion Hutsell <st.gish@gmail.com>
2020-08-03 12:00:37 -0600

    Incomplete User story 4

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -8,4 +8,15 @@ class Flight <ApplicationRecord
                         :time,
                         :departure_city,
                         :arrival_city
+
+  def adults
+    binding.pry
+    passengers.where("age >= 18")
+    Flight.joins(:passengers).group(:id).where('age >= 18')
+    # Flight.where("number = :number", number: params[:flight_number])
+  end
+
+  def minors
+
+  end
 end
--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -3,28 +3,48 @@ require 'rails_helper'
 RSpec.describe 'Flights show page', type: :feature do
   before(:each) do
     @airline = Airline.create!(name: "Delta")
-    @flight = @airline.flights.create!(number: "1727", date: "08-03-20", time: "3:30pm MST", departure_city: "Denver", arrival_city: "Reno")
+    @flight1 = @airline.flights.create!(number: "1727", date: "08-03-20", time: "3:30pm MST", departure_city: "Denver", arrival_city: "Reno")
+    @flight2 = @airline.flights.create!(number: "7777", date: "08-03-20", time: "8:30pm MST", departure_city: "Reno", arrival_city: "Seattle")
+    @flight3 = @airline.flights.create!(number: "1776", date: "08-04-20", time: "8:30am PST", departure_city: "Seattle", arrival_city: "Houston")
+
     @joe = Passenger.create!(name: "Joe", age: "7")
     @bob = Passenger.create!(name: "Bob", age: "77")
+    @joey = Passenger.create!(name: "Joey", age: "7")
+    @bobby = Passenger.create!(name: "Bobby", age: "77")
 
 
-    FlightPassenger.create!(flight: @flight, passenger: @joe)
-    FlightPassenger.create!(flight: @flight, passenger: @bob)
+    FlightPassenger.create!(flight: @flight1, passenger: @joe)
+    FlightPassenger.create!(flight: @flight1, passenger: @bob)
+
+    FlightPassenger.create!(flight: @flight1, passenger: @joey)
+    FlightPassenger.create!(flight: @flight1, passenger: @bobby)
+
+    FlightPassenger.create!(flight: @flight2, passenger: @joe)
+    FlightPassenger.create!(flight: @flight2, passenger: @bob)
 
 
   end
   it 'shows flight info' do
-    visit "/flights/#{@flight.id}"
+    visit "/flights/#{@flight1.id}"
 
-    expect(page).to have_content(@flight.number)
-    expect(page).to have_content(@flight.date)
-    expect(page).to have_content(@flight.time)
-    expect(page).to have_content(@flight.departure_city)
-    expect(page).to have_content(@flight.arrival_city)
+    expect(page).to have_content(@flight1.number)
+    expect(page).to have_content(@flight1.date)
+    expect(page).to have_content(@flight1.time)
+    expect(page).to have_content(@flight1.departure_city)
+    expect(page).to have_content(@flight1.arrival_city)
     expect(page).to have_content(@airline.name)
     expect(page).to have_content("Joe")
     expect(page).to have_content("Bob")
 
+  end
+
+  it "shows number of minors and adults" do
+    visit "/flights/#{@flight1.id}"
+
+
+    expect(page).to have_content("Adults: 2")
+
+    expect(page).to have_content("Minors: 2")
   end
 
 end
